1658b592a834615c2dd5f37c6870cee6549b1f62
rohith.radhakrishnan <rohith.radhakrishnan@gm.com>
2024-01-19 15:49:24 -0500

    add testing off

--- .github/workflows/build_smtk.sh
+++ .github/workflows/build_smtk.sh
@@ -1,6 +1,7 @@
 export INSTALL_DIR=/opt/axion/axionsdk
 export CCACHE_DIR=/opt/axion/ccache
 ls $INSTALL_DIR/lib/cmake
+git submodule update --init --recursive
 cmake                                                               \
     -G Ninja                                                        \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                             \
@@ -8,5 +9,5 @@ cmake                                                               \
     $SRC_DIR \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache  \
     -DBUILD_TESTING=OFF \
-    -DQt5_DIR=$INSTALL_DIR/lib/cmake/Qt5
-
+    -DQt5_DIR=$INSTALL_DIR/lib/cmake/Qt5 \
+    -DDelaunay_ENABLE_TESTING=OFF
