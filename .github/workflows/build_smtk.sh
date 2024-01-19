52fda11e23170006b6065f53d9a5f4b3ba64c813
rohith.radhakrishnan <rohith.radhakrishnan@gm.com>
2024-01-19 15:46:33 -0500

    add testing off

--- .github/workflows/build_smtk.sh
+++ .github/workflows/build_smtk.sh
@@ -1,7 +1,12 @@
 export INSTALL_DIR=/opt/axion/axionsdk
+export CCACHE_DIR=/opt/axion/ccache
 ls $INSTALL_DIR/lib/cmake
 cmake                                                               \
     -G Ninja                                                        \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                             \
     -DCMAKE_PREFIX_PATH="$INSTALL_DIR/lib/cmake/Boost-1.79.0;$INSTALL_DIR/lib/cmake/paraview-5.11;$INSTALL_DIR/lib/cmake/paraview-5.11/vtk"       \
-    $SRC_DIR
+    $SRC_DIR \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache  \
+    -DBUILD_TESTING=OFF \
+    -DQt5_DIR=$INSTALL_DIR/lib/cmake/Qt5
+
