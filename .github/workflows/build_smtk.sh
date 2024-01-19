723eddbe3fc649cf865ae6eceb148f4eb20a8c03
rohith.radhakrishnan <rohith.radhakrishnan@gm.com>
2024-01-19 09:46:43 -0500

    Fix smtk

--- .github/workflows/build_smtk.sh
+++ .github/workflows/build_smtk.sh
@@ -2,5 +2,5 @@ export INSTALL_DIR=/opt/axion/axionsdk
 cmake                                                               \
     -G Ninja                                                        \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                             \
-    -DCMAKE_PREFIX_PATH="$INSTALL_DIR/lib/cmake/Boost-1.79.0"       \
+    -DCMAKE_PREFIX_PATH="$INSTALL_DIR/lib/cmake/Boost-1.79.0;$INSTALL_DIR/lib/cmake/paraview-5.11"       \
     $SRC_DIR
