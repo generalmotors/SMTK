123c8a9b68e44aa2a69fd9f0cad2618dc3b32234
rohith.radhakrishnan <rohith.radhakrishnan@gm.com>
2024-01-19 09:44:31 -0500

    Use src dir

--- .github/workflows/build_smtk.sh
+++ .github/workflows/build_smtk.sh
@@ -2,4 +2,5 @@ export INSTALL_DIR=/opt/axion/axionsdk
 cmake                                                               \
     -G Ninja                                                        \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                             \
+    -DCMAKE_PREFIX_PATH="$INSTALL_DIR/lib/cmake/Boost-1.79.0"       \
     $SRC_DIR
