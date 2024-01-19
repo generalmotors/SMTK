a5cb2d2fc8cd03be96d46f0aa415cca331878513
rohith.radhakrishnan <rohith.radhakrishnan@gm.com>
2024-01-19 09:39:43 -0500

    Use src dir

--- .github/workflows/build_smtk.sh
+++ .github/workflows/build_smtk.sh
@@ -1,5 +1,4 @@
 export INSTALL_DIR=/opt/axion/axionsdk
-export SRC_DIR= ${{ github.workspace }}
 cmake                                                               \
     -G Ninja                                                        \
     -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                             \
