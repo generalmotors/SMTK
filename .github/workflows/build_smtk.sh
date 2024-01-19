412360edd838ab1f4c0a238643cfc6b9320873b4
rohith.radhakrishnan <rohith.radhakrishnan@gm.com>
2024-01-19 09:25:03 -0500

    Build smtk

--- .github/workflows/build_smtk.sh
+++ .github/workflows/build_smtk.sh
@@ -1,1 +1,6 @@
 export INSTALL_DIR=/opt/axion/axionsdk
+export SRC_DIR= ${{ github.workspace }}
+cmake                                                               \
+    -G Ninja                                                        \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR                             \
+    $SRC_DIR
